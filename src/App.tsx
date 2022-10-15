2b26590ebdb44c47468153b3c4c935c2ef29fbb3
m-goos <10080192+m-goos@users.noreply.github.com>
2022-10-15 22:19:30 +0200

    feat: add and set up react-query and react-query-devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import './App.css';
 import Filter from './components/Filter';
 import Navbar from './components/Navbar';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div className="h-screen bg-gray-100 sm:bg-orange-600">
-      <Navbar />
-      <Filter />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="h-screen bg-gray-100 sm:bg-orange-600">
+        <Navbar />
+        <Filter />
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
