f681e8cbc375180312d8474ec5ede77cbeb781aa
m-goos <10080192+m-goos@users.noreply.github.com>
2022-10-16 21:18:23 +0200

    feat: improve styling by aligningfilter buttons on the right

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -15,16 +15,20 @@ function Filter() {
   return (
     <div className="flex items-center py-2 px-4 shadow-md">
       <div className="flex w-full items-center justify-between">
-        <AdjustmentsHorizontalIcon className="block h-6 w-6" />
-        <span className="hidden text-2xl font-medium sm:block">Filters</span>
-        <RadioGroupButtons
-          options={financialFilterOptions}
-          onChange={() => dispatch({ type: 'changeFinancialFilter' })}
-        />
-        <RadioGroupButtons
-          options={timeFilterOptions}
-          onChange={() => dispatch({ type: 'changeTimeFilter' })}
-        />
+        <div className="flex items-center gap-2">
+          <AdjustmentsHorizontalIcon className="block h-6 w-6" />
+          <span className="hidden text-2xl font-medium sm:block">Filters</span>
+        </div>
+        <div className="flex gap-2">
+          <RadioGroupButtons
+            options={financialFilterOptions}
+            onChange={() => dispatch({ type: 'changeFinancialFilter' })}
+          />
+          <RadioGroupButtons
+            options={timeFilterOptions}
+            onChange={() => dispatch({ type: 'changeTimeFilter' })}
+          />
+        </div>
       </div>
     </div>
   );
