8296ee725236d4c77391a8354a0d0b43f592e161
m-goos <10080192+m-goos@users.noreply.github.com>
2022-10-16 23:25:15 +0200

    feat: move react-table starter values to InvoiceTable to start re-usable component

--- src/components/InvoiceTable.tsx
+++ src/components/InvoiceTable.tsx
@@ -1,11 +1,81 @@
+import { createColumnHelper } from '@tanstack/react-table';
+
 import Table from './Table';
+
+const columnHelper = createColumnHelper<Person>();
+
+type Person = {
+  firstName: string;
+  lastName: string;
+  age: number;
+  visits: number;
+  status: string;
+  progress: number;
+};
+
+const columns = [
+  columnHelper.accessor('firstName', {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor((row) => row.lastName, {
+    id: 'lastName',
+    cell: (info) => <i>{info.getValue()}</i>,
+    header: () => <span>Last Name</span>,
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('age', {
+    header: () => 'Age',
+    cell: (info) => info.renderValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('visits', {
+    header: () => <span>Visits</span>,
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('status', {
+    header: 'Status',
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('progress', {
+    header: 'Profile Progress',
+    footer: (info) => info.column.id,
+  }),
+];
+
+const defaultData: Person[] = [
+  {
+    firstName: 'tanner',
+    lastName: 'linsley',
+    age: 24,
+    visits: 100,
+    status: 'In Relationship',
+    progress: 50,
+  },
+  {
+    firstName: 'tandy',
+    lastName: 'miller',
+    age: 40,
+    visits: 40,
+    status: 'Single',
+    progress: 80,
+  },
+  {
+    firstName: 'joe',
+    lastName: 'dirte',
+    age: 45,
+    visits: 20,
+    status: 'Complicated',
+    progress: 10,
+  },
+];
 
 /**
  * @returns List of the 15 latest invoices by date, with id, date, costumer name, region, invoice total
 (or total margin, depending on switcher value).
  */
 function InvoiceTable() {
-  return <Table />;
+  return <Table data={defaultData} columns={columns} />;
 }
 
 export default InvoiceTable;
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,94 +1,18 @@
 import {
-  createColumnHelper,
   flexRender,
   getCoreRowModel,
   useReactTable,
 } from '@tanstack/react-table';
-import { useEffect, useReducer, useState } from 'react';
+import { useReducer } from 'react';
 
-type Person = {
-  firstName: string;
-  lastName: string;
-  age: number;
-  visits: number;
-  status: string;
-  progress: number;
-};
-
-// interface TableProps {
-//   columns: any;
-//   data: any;
-// }
-
-const defaultData: Person[] = [
-  {
-    firstName: 'tanner',
-    lastName: 'linsley',
-    age: 24,
-    visits: 100,
-    status: 'In Relationship',
-    progress: 50,
-  },
-  {
-    firstName: 'tandy',
-    lastName: 'miller',
-    age: 40,
-    visits: 40,
-    status: 'Single',
-    progress: 80,
-  },
-  {
-    firstName: 'joe',
-    lastName: 'dirte',
-    age: 45,
-    visits: 20,
-    status: 'Complicated',
-    progress: 10,
-  },
-];
-
-const columnHelper = createColumnHelper<Person>();
-
-const columns = [
-  columnHelper.accessor('firstName', {
-    cell: (info) => info.getValue(),
-    footer: (info) => info.column.id,
-  }),
-  columnHelper.accessor((row) => row.lastName, {
-    id: 'lastName',
-    cell: (info) => <i>{info.getValue()}</i>,
-    header: () => <span>Last Name</span>,
-    footer: (info) => info.column.id,
-  }),
-  columnHelper.accessor('age', {
-    header: () => 'Age',
-    cell: (info) => info.renderValue(),
-    footer: (info) => info.column.id,
-  }),
-  columnHelper.accessor('visits', {
-    header: () => <span>Visits</span>,
-    footer: (info) => info.column.id,
-  }),
-  columnHelper.accessor('status', {
-    header: 'Status',
-    footer: (info) => info.column.id,
-  }),
-  columnHelper.accessor('progress', {
-    header: 'Profile Progress',
-    footer: (info) => info.column.id,
-  }),
-];
+interface TableProps {
+  columns: any;
+  data: any;
+}
 
 // from: https://tanstack.com/table/v8/docs/examples/react/basic
-// function Table({ columns, data }: TableProps) {
-function Table() {
-  const [data, setData] = useState(() => [...defaultData]);
+function Table({ columns, data }: TableProps) {
   const rerender = useReducer(() => ({}), {})[1];
-
-  useEffect(() => {
-    // this will be an API call
-    setData([...defaultData]);
-  }, []);
 
   const table = useReactTable({
     data,
