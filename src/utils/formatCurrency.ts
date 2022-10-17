c41acd359f756f17ea1c1849775a6848a6cf2df9
m-goos <10080192+m-goos@users.noreply.github.com>
2022-10-17 23:28:23 +0200

    feat: add functionality to hide decimals

--- src/utils/formatCurrency.ts
+++ src/utils/formatCurrency.ts
@@ -1,5 +1,8 @@
-export const formatCurrency = (number: number) =>
+/** optionally removes decimals */
+export const formatCurrency = (number: number, noDecimals?: boolean) =>
   Intl.NumberFormat('nl-NL', {
     style: 'currency',
     currency: 'EUR',
+    maximumFractionDigits: noDecimals ? 0 : 2,
+    minimumFractionDigits: noDecimals ? 0 : 2,
   }).format(number);
