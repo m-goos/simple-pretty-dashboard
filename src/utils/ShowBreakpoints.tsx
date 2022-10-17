b60685c8257543402320ba02d2ddb9e38683b856
m-goos <10080192+m-goos@users.noreply.github.com>
2022-10-17 20:09:42 +0200

    feat: add small utility for showing current breakpoint

---
+++ src/utils/ShowBreakpoints.tsx
@@ -0,0 +1,13 @@
+function ShowBreakpoints() {
+  return (
+    <>
+      <span className="sm:hidden">XS</span>
+      <span className="hidden sm:block md:hidden">SM</span>
+      <span className="hidden md:block lg:hidden">MD</span>
+      <span className="hidden lg:block xl:hidden">LG</span>
+      <span className="hidden xl:block">XL</span>
+    </>
+  );
+}
+
+export default ShowBreakpoints;
