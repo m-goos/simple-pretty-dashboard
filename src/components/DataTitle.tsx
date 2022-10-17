ed1077038e6b14f0280baecd7f80ed7e81c867ab
m-goos <10080192+m-goos@users.noreply.github.com>
2022-10-17 20:06:51 +0200

    feat: add icon to title

--- src/components/DataTitle.tsx
+++ src/components/DataTitle.tsx
@@ -1,9 +1,17 @@
+import { ReactNode } from 'react';
+
 interface DataTitleProps {
   title: string;
+  icon: ReactNode;
 }
 
-function DataTitle({ title }: DataTitleProps) {
-  return <div className="py-2 text-xl font-medium">{title}</div>;
+function DataTitle({ title, icon }: DataTitleProps) {
+  return (
+    <div className="flex items-center py-2 px-1 text-xl font-medium">
+      <span className="h-7 w-7 pr-2">{icon}</span>
+      <span className="pb-2">{title}</span>
+    </div>
+  );
 }
 
 export default DataTitle;
