a2ad1f8dfb716427522f3342811417e92f06d830
m-goos <10080192+m-goos@users.noreply.github.com>
2022-10-18 01:12:17 +0200

    refactor: clean up comments

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,17 +35,7 @@ function Navbar() {
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
                   <div className="block h-8 w-auto lg:hidden"></div>
-                  {/* <img
-                    className="block h-8 w-auto lg:hidden"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  /> */}
                   <div className="hidden h-8 w-auto lg:block"></div>
-                  {/* <img
-                    className="hidden h-8 w-auto lg:block"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  /> */}
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
